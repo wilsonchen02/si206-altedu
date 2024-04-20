a7a360d0e87c7ef96f591fe69c0f45f5b6be4253
wilsonchen02 <wilsonchen02@gmail.com>
2024-04-20 01:30:36 -0400

    geodb can now fetch page after page of results

--- geodbAPI.py
+++ geodbAPI.py
@@ -5,6 +5,7 @@ import json
 # Particularly [name, lat, long, elevation, population, region]
 # of cities in the US
 # NOTE: the free API limits to 10 results per request
+# NOTE: trying to fetch an amount N > max results will return max results
 
 # This uses a public API key
 # base_url = "http://geodb-free-service.wirefreethought.com/v1/geo/"
@@ -14,60 +15,112 @@ header = {
     "content-type": "application/json"
 }
 
-query = """
-query {
-    country(id:"US") {
-        name
-        populatedPlaces {
-            totalCount
-            edges {
-                node {
-                    name
-                    latitude
-                    longitude
-                    elevationMeters
-                    population
-                    region {
+first_query = """
+    query {
+        country(id: "US") {
+            name
+            populatedPlaces(types: ["CITY"], first: 10) {
+                totalCount
+                pageInfo {
+                    endCursor
+                    hasNextPage
+                }
+                edges {
+                    cursor
+                    node {
                         name
+                        latitude
+                        longitude
+                        elevationMeters
+                        population
+                        region {
+                            name
+                        }
                     }
                 }
             }
         }
     }
-}
-"""
+    """
 
-response = requests.post(url=graphql_url, json={"query": query}, headers=header)
-print("response status code: ", response.status_code)
+query = """
+    query($prev_cursor: String) {
+        country(id: "US") {
+            name
+            populatedPlaces(types: ["CITY"], first: 10, after: $prev_cursor) {
+                totalCount
+                pageInfo {
+                    endCursor
+                    hasNextPage
+                }
+                edges {
+                    cursor
+                    node {
+                        name
+                        latitude
+                        longitude
+                        elevationMeters
+                        population
+                        region {
+                            name
+                        }
+                    }
+                }
+            }
+        }
+    }
+    """
 
-if response.status_code == 200: 
-    data = response.json()
-    print("response: ", response.content)
-else:
-    print("Failed to retrieve data:", response.status_code)
-    
-# TODO: have this fill up the DB instead of local json (no caching allowed)
-with open("test_geodb_data.json", "w", encoding="utf-8") as f:
-    json.dump(data, f)
+# TODO: first req should not have the "after" parameter for populatedPlaces
+# Use GraphQL pagination to get the next 10 cities
+def geodb_update_data():
+    data = {}
+    prev_cursor = ""
+    hasNextPage = True
+    is_first_query = True
 
-# Fetch data from API and store into db
-# Can only 
-def geodb_update():
-    pass
-    # Obtain all city IDs in the US (cities?countryIds=US)
-    
-    
-    # Obtain elevation of places (geo/places)
-    
-
-# Fetch stored data from db
-
+    while hasNextPage:
+        if is_first_query:
+            # Perform first query (try first 10 or less)
+            response = requests.post(url=graphql_url, json={"query": first_query}, headers=header)
+            is_first_query = False
+        else:
+            # Not the first query (use prev_cursor as offset for next page)
+            variables = {
+                "prev_cursor": prev_cursor
+            }
+            response = requests.post(url=graphql_url, json={"query": query, "variables": variables}, headers=header)
+            
+        # Check if response is ok
+        # If response is ok but GraphQL response isn't, catch it
+        if response.status_code == 200:
+            data = response.json()
+            print("response:", response.content)    # TODO: remove this after done debugging
+            
+            if data.get("errors") != None:
+                print("GraphQL query error")
+                return
+            
+            with open("test_geodb_data.json", "w", encoding="utf-8") as f:
+                json.dump(data, f)
+            
+            # Check if there's another page available. If so, prepare to get the next one
+            hasNextPage = data["data"]["country"]["populatedPlaces"]["pageInfo"]["hasNextPage"]        
+            # Grab value of last city's cursor in the previous batch
+            prev_cursor = data["data"]["country"]["populatedPlaces"]["pageInfo"]["endCursor"]
+            
+            # Remove duplicates (Los Angeles has 2 entries but different ids, use 2 columns to determine uniqueness)
+            # TODO: have this fill up the DB instead of local json (no caching allowed)
+            
+        else:
+            # Response is bad
+            print("Failed to retrieve data:", response.status_code)
+            return
 
 # TODO: use this to test funcs
 def main():
-    # geodb_update()
+    geodb_update_data()
     pass
-
 
 if __name__ == "__main__":
     main()
