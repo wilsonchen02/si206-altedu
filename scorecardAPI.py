446c8e810594415e21187983b62c15916dfc676f
leolind <leolind@umich.edu>
2024-04-27 16:11:04 -0400

    calculations file

--- scorecardAPI.py
+++ scorecardAPI.py
@@ -6,6 +6,63 @@ import sys
 api_key = "YOn4vJlPGcmk32VfxDWB0VvaMDiVNKZ5c1w95CoV"
 
 
+def convert_state_id(state):
+    state_dict = {
+        "AL" : 0,
+        "AK": 1,
+        "AZ": 2,
+        "AR": 3,
+        "CA": 4,
+        "CO": 5,
+        "CT": 6,
+        "DE": 7,
+        "FL": 8,
+        "GA": 9,
+        "HI": 10,
+        "ID": 11,
+        "IL": 12,
+        "IN": 13,
+        "IA": 14,
+        "KS": 15,
+        "KY": 16,
+        "LA": 17,
+        "ME": 18,
+        "MD": 19,
+        "MA": 20,
+        "MI": 21,
+        "MN": 22,
+        "MS": 23,
+        "MO": 24,
+        "MT": 25,
+        "NE": 26,
+        "NV": 27,
+        "NH": 28,
+        "NJ": 29,
+        "NM": 30,
+        "NY": 31,
+        "NC": 32,
+        "ND": 33,
+        "OH": 34,
+        "OK": 35,
+        "OR": 36,
+        "PA": 37,
+        "RI": 38,
+        "SC": 39,
+        "SD": 40,
+        "TN": 41,
+        "TX": 42,
+        "UT": 43,
+        "VT": 44,
+        "VA": 45,
+        "WA": 46,
+        "WV": 47,
+        "WI": 48,
+        "WY": 49,
+    }
+    if state not in state_dict:
+        exit(1)
+    return state_dict[state]
+
 def check_state(input):
     us_state_abbreviations = [
         "AL",
