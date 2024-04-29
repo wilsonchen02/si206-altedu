ccc0c1c25dd381fcc20bed77423b692dc4c5ee0e
leolind <leolind@umich.edu>
2024-04-29 19:13:51 -0400

    Second visualizion kinda done, needs review

--- visualization.py
+++ visualization.py
@@ -2,7 +2,31 @@ import pandas as pd
 import plotly.express as px
 
 def bar_graph(df):
-    pass
+    df["student_percentage"] = (
+        df["student_percentage"].str.replace("%", "").astype(float)
+    )
+    df = df[df["student_percentage"] < 100]
+    df = df.sort_values(by="student_percentage", ascending=True)
+    fig = px.bar(
+        df, x="city", y="student_percentage", title="Student Percentage by City"
+    )
+    fig.update_layout(
+        xaxis_title="City",
+        yaxis_title="Student Percentage",
+        plot_bgcolor="lightgray",  # Optional: Change the plot background color
+        paper_bgcolor="whitesmoke",  # Optional: Change the overall figure background color
+    )
+    fig.update_layout(
+        plot_bgcolor="#343d46", 
+        paper_bgcolor="whitesmoke", 
+        xaxis_title="City Name",
+        yaxis_title="Student Percentage"
+    )
+    fig.update_xaxes(linewidth=0.1, gridcolor="#4f5b66")
+    fig.update_yaxes(linewidth=0.1, gridcolor="#4f5b66")
+    fig.update_traces(marker_color="blue")
+
+    fig.show()
 
 def line_graph(df):
     bins = range(
@@ -33,12 +57,11 @@ def line_graph(df):
     fig.update_yaxes(linewidth=0.1, gridcolor="#4f5b66")
     fig.show()
 
-
 def main():
     df = pd.read_csv("calculations.csv")
     line_graph(df)
     bar_graph(df)
-    
+
 
 if __name__ == "__main__":
     main()
